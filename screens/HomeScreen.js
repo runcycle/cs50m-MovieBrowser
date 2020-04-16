6b39e5f7121b374af7268d15c0c7695776aee92b
Justin Bartholomew <jbartholomew@turinogroup.com>
2020-04-16 16:36:31 -0400

    Beginning to set-up, add navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,51 @@
+import React from './node_modules/react';
+import { StyleSheet, TextInput, View } from 'react-native';
+import { createStackNavigator } from './node_modules/@react-navigation/stack';
+import { Constants } from 'expo';
+
+export default class MovieBrowser extends React.Component {
+    state = {
+        title: ""
+    }
+    render() {
+        return (
+          <View behavior="padding" style={styles.container}>
+            <TextInput
+              style={styles.input}
+              value={this.state.title}
+              // onChangeText={}
+              placeholder="Movie Title"
+            />
+          </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        paddingTop: Constants.statusBarHeight,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: 'black',
+        minWidth: 100,
+        marginTop: 20,
+        marginHorizontal: 20,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderRadius: 3,
+      },
+  });
+  
+  const AppNavigator = createStackNavigator({
+    Home: HomeScreen,
+    Results: ResultsScreen
+   },
+   {
+     initialRouteName: "HomeScreen"
+   });
---
+++ screens/ResultsScreen.js
@@ -0,0 +1,4 @@
+import React from './node_modules/react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from './node_modules/@react-navigation/native';
+import { createStackNavigator } from './node_modules/@react-navigation/stack';
