c6cfc4546c4192a269ed1296c2f956cbb15724d3
Justin Bartholomew <jbartholomew@turinogroup.com>
2020-04-20 09:40:44 -0400

    4/20 App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import MovieBrowser from "./screens/HomeScreen.js"
 
-
 export default class App extends Component {
   render() {
       return <MovieBrowser />
