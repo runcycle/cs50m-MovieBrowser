c68295e52087aa6d2a846b81297386b28357c637
runcycle <justinbartholomew@gmail.com>
2020-05-17 07:35:36 -0400

    Update to HomeScreen and DetailsScreen

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Constants from 'expo-constants';
-//import { NavigationContainer } from './node_modules/@react-navigation/native';
-//import { createStackNavigator } from './node_modules/@react-navigation/stack';
-//import MovieCard from "../MovieCard";
+import MovieCard from "../MovieCard";
 
 export default class DetailsScreen extends React.Component {
     static navigationOptions = {
