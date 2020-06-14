4d42005b558d43b272941af603fe1b313023cf85
runcycle <justinbartholomew@gmail.com>
2020-06-14 07:52:09 -0400

    Change to DS

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -8,80 +8,37 @@ export default class DetailsScreen extends React.Component {
         movie: movie
    }
 
-<<<<<<< HEAD
-   getID = ({ route }) => {
-       const { id } = route.params;
-       console.log({id})
-   }
-   /*
    getDetails = async (id) => {
-        const result = await fetchDetails(id);
-        this.setState({ movie: result })
-        console.log(this.state.movie)
+    const result = await fetchDetails(id);
+    this.setState({ movie: result })
+    console.log(this.state.movie)
     }
-    */
-   
-    componentDidMount() {
-        this.getID();
-       //this.getDetails(this.state.id);
-   }
-   
-    render () {
-=======
-    render () {
-<<<<<<< HEAD
-        //const { navigation } = this.props;
-        const { route } = this.props;
-        const { movie: params } = route.params;
-        const movie = this.state.movie;
-
-        getDetails = async (id) => {
-            const result = await fetchDetails(id);
-            this.setState({ movie: result })
-            console.log(this.state.movie)
-        }
-        
-        if (this.state.movie.imdbID === 0) {
-            getDetails(params.imdbID)
-        }
-
+    
+    render() {
         return (
-        <Text>id: {JSON.stringify(id)}</Text>
-=======
->>>>>>> ebcc8e6da3e58ff1ccf90040385148c00e08d2eb
-        //const movie = this.state.movie
-        //const { navigation } = this.props;
-
-        return (
-<<<<<<< HEAD
-        <Text>id: </Text>
-=======
-        <Text>id: {JSON.stringify(this.props.navigation.getParam("id"))}</Text>
->>>>>>> c812f4a0162bae69724771a162f3593fb39bc47a
->>>>>>> ebcc8e6da3e58ff1ccf90040385148c00e08d2eb
-            /*
-            <SafeAreaView style={{ flex: 1 }}>
-                <ScrollView style={styles.container}>
-                    <Text>Movie Details</Text>
-                    <Image style={styles.image} 
-                        source={{ uri: movie.Poster }}
-                    />
-                    <Text>{ movie.Title }</Text>
-                    <Text>{ movie.Year }</Text>
-                    <Text>{ movie.Rated }</Text>
-                    <Text>{ movie.Runtime }</Text>
-                    <Text>{ movie.Director }</Text>
-                    <Text>{ movie.Actors }</Text>
-                    <Text>{ movie.Plot }</Text>
-                    <Text>{ movie.imdbRating }</Text>
-                    <Text>{ movie.BoxOffice }</Text>
-                    <Text>{ movie.Awards }</Text>
-                </ScrollView>
-            </SafeAreaView>
-            */
+            <Text>id: </Text>
+                <SafeAreaView style={{ flex: 1 }}>
+                    <ScrollView style={styles.container}>
+                        <Text>Movie Details</Text>
+                        <Image style={styles.image} 
+                            source={{ uri: movie.Poster }}
+                        />
+                        <Text>{ movie.Title }</Text>
+                        <Text>{ movie.Year }</Text>
+                        <Text>{ movie.Rated }</Text>
+                        <Text>{ movie.Runtime }</Text>
+                        <Text>{ movie.Director }</Text>
+                        <Text>{ movie.Actors }</Text>  
+                        <Text>{ movie.Plot }</Text>
+                        <Text>{ movie.imdbRating }</Text>
+                        <Text>{ movie.BoxOffice }</Text>
+                        <Text>{ movie.Awards }</Text>
+                    </ScrollView>
+                </SafeAreaView>
         );
     }
 }
+
 
 const styles = StyleSheet.create({
     container: {
